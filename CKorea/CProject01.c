39ece3a8460f3aef99b42e62c298601942519d0f
김동환 <tkdel1123@naver.com>
2022-04-27 01:48:28 +0900

    추가하기

--- CKorea/CProject01.c
+++ CKorea/CProject01.c
@@ -1,7 +1,7 @@
 //프로그래밍 언어:정해진 규칙으로 작성하면 기계에게 말을 시킬 수 있는것
 #include <stdio.h>							//printf 화면에 보여주는 기능을 사용하기 위해서 추가해야하는 파일
 
-void main() {								//main:시작위치
-	printf("안녕하세요~C언어");
-
+void main1() {								//main:시작위치
+	printf("안녕하세요~C언어");				//:세미콜론,문장이 끝날때 해줘야하는 표시		
+	ptintf("\nHello CWorld");				//\n:한줄띄우기
 }					//중괄호:범위
